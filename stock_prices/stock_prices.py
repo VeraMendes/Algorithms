141359dfacc9b3ffe064696f1fadeb743a6c1988
VeraMendes <veramendes10@gmail.com>
2020-03-18 16:05:01 -0700

    first problem solved

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,53 @@
 
 import argparse
 
+# daily-trading Amazon Stock
+# input -> list of values
+# output -> 1 value (max profit that can be made for a single buy and sell)
+# max difference between smallest and largest in the list
+# computed as a subtraction between a value and another value that comes before it
+
+# keep checking current_min_price_so_far and max_profit_so_far
+
+# plan
+# find max_value of list and index (cannot be the 0 index)
+# find min_value of list up to max_value_index (cannot be index higher than max)
+# then subtract both numbers
+
+
+a = [100, 90, 80, 50, 20, 10]
+b = [10, 7, 5, 8, 11, 9]
+c = [1050, 270, 1540, 3800, 2]
+d = [100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]
+e = [10000, 1000, 5900, 50, 5000, 10]
+
+
+# for i, v in enumerate(a):
+#     max_value = max(a[1:])
+#     print(max_value[i])
+
+# max_value = max(prices[1:])
+# max_value_index = max_value[a]
+# min_value = min(prices[:max_value_index])
+# result = max_value - min_value
+
+
 def find_max_profit(prices):
-  pass
+
+    if len(prices) < 2:
+        return "Not enough values to calculate profit"
+    else:
+        my_list = []
+        for i, a in enumerate(prices):
+            for b in prices[i+1:]:
+
+                profit = b - a
+                my_list.append(profit)
+
+        result = max(my_list)
+
+    return result
+
 
 
 if __name__ == '__main__':
